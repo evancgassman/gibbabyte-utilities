76f2013aa892767aff1e5a750210bc30e0fb5229
Evan Gassman <102568007+evancgassman@users.noreply.github.com>
2022-04-14 18:35:18 -0400

    Add files via upload

---
+++ GibbabyteUtilitiesRequest.js
@@ -0,0 +1,24 @@
+//   ___ ___ ___ ___   _   _____   _______ ___ 
+//  / __|_ _| _ ) _ ) /_\ | _ ) \ / /_   _| __|
+// | (_ || || _ \ _ \/ _ \| _ \\ V /  | | | _|  
+//  \___|___|___/___/_/ \_\___/ |_|   |_| |___|
+//  By Evan Gassman
+
+function GibbabyteUtilitiesRequest(APIKEY, FUNCTION) {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://utilities.gibbabyte.us/api.php");
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }};
+   
+    xhr.send(`{"api_key": "`+APIKEY+`", "function": "`+FUNCTION+`"}`);
+
+}
+
+if(module) module.exports = GibbabyteUtilitiesRequest();
