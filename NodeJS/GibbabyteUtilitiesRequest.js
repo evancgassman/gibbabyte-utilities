a7b3058fd4f1f1fd2de734f4fd2b99947e86e35c
Evan Gassman <102568007+evancgassman@users.noreply.github.com>
2022-04-15 11:49:02 -0400

    Add files via upload

---
+++ NodeJS/GibbabyteUtilitiesRequest.js
@@ -0,0 +1,23 @@
+//   ___ ___ ___ ___   _   _____   _______ ___ 
+//  / __|_ _| _ ) _ ) /_\ | _ ) \ / /_   _| __|
+// | (_ || || _ \ _ \/ _ \| _ \\ V /  | | | _|  
+//  \___|___|___/___/_/ \_\___/ |_|   |_| |___|
+//  By Evan Gassman
+
+function GibbabyteUtilitiesRequest(APIKEY, FUNCTION) {
+    
+    const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://utilities.gibbabyte.us/api.php");
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }};
+   
+    xhr.send(`{"api_key": "`+APIKEY+`", "function": "`+FUNCTION+`"}`);
+
+}
